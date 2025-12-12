8f5d70c5aacee1a3bb713ccbdd276d72c9dc440c
CXDezign <29733018+CXDezign@users.noreply.github.com>
2025-12-12 23:46:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,35 @@
 #!/bin/bash -ex
 
-if [ $(grep -ci $USERNAME /etc/shadow) -eq 0 ]; then
-    useradd -r -G lpadmin -M $USERNAME
+# Environment Variables
+set -e
 
-    # Add Username:Password
+# PPDs
+mv ./ppd/cnijfilter2_6.80-1_${TARGETARCH}.deb /tmp/cnijfilter2.deb
+apt install -y /tmp/cnijfilter2.deb
+
+# Default Configuration
+/usr/sbin/cupsd \
+  && while [ ! -f /var/run/cups/cupsd.pid ]; do sleep 1; done \
+  && cupsctl --remote-admin --remote-any --share-printers \
+  && kill $(cat /var/run/cups/cupsd.pid)
+echo "ServerAlias *" >> /etc/cups/cupsd.conf
+echo "DefaultEncryption Never" >> /etc/cups/cupsd.conf
+
+# Environment Configuration
+if [ $(grep -ci $USERNAME /etc/shadow) -eq 0 ]; then
+    # User Configuration
+    useradd -r -G lpadmin -M $USERNAME
     echo $USERNAME:$PASSWORD | chpasswd
 
-    # Add Timezone
+    # Timezone Configuration
     ln -fs /usr/share/zoneinfo/$TIMEZONE /etc/localtime
     dpkg-reconfigure --frontend noninteractive tzdata
 fi
 
-# Restore default CUPS config if empty
+# Restore Configurations
 if [ ! -f /etc/cups/cupsd.conf ]; then
     cp -rpn /etc/cups.bak/* /etc/cups/
 fi
 
+# Execute
 exec /usr/sbin/cupsd -f
